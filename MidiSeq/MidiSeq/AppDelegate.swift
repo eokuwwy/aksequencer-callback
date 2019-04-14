cb288bd3c3c78ca038f7214c07b44708256af68d
Mark <markjeschke@yahoo.com>
2019-04-14 14:49:29 -0700

    Placed AudioKit start/stop into its own Conductor class.

--- MidiSeq/MidiSeq/AppDelegate.swift
+++ MidiSeq/MidiSeq/AppDelegate.swift
@@ -7,19 +7,17 @@
 //
 
 import UIKit
-import AudioKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var conductor = Conductor.sharedInstance
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        if AKSettings.appSupportsBackgroundAudio {
-            AKSettings.playbackWhileMuted = true
-        }
+        
+        // Start AudioKit upon launch
+        conductor.startAudioKitEngine()
         
         return true
     }
@@ -32,20 +30,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        do {
-            try AudioKit.start()
-        } catch {
-            print("Error starting AudioKit")
-        }
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        do {
-            try AudioKit.stop()
-        } catch {
-            print("Error stopping AudioKit")
-        }
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
@@ -54,6 +42,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
+        // Stop the AudioKit engine, if the app terminates.
+        conductor.stopAudioKitEngine()
     }
 
 
